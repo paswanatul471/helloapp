4af74e1b69cfb3666dd4dca11803b4de268c865a
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2023-06-17 17:08:03 +0530

    Using API to load data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 function App() {
-  const[checked, setChecked] = useState(false);
-
+  const [data, setData] = useState(null);
+  useEffect(()=>{
+    fetch(
+      `https://newsapi.org/v2/everything?q=tesla&from=2023-05-17&sortBy=publishedAt&apiKey=12b57f3f5a6b4ba0a04e4df8a2627c02`
+    )
+    .then((response)=> response.json())
+    .then(setData);
+  },[]);
+if(data)
+  return(
+    <pre>{JSON.stringify(data,null,2)}</pre>
+  )
   return (
     <>
-    <input type="checkbox" value={checked} onChange={()=>setChecked((checked)=>!checked)} />
-    <label >
-      {checked ? "checked" : "not checked"}
-    </label>
+    <h1>
+      Data
+    </h1>
     
     </>
   );
