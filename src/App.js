b84d249874e97165e70bee904c46e2d59802c2c7
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2023-06-18 11:33:02 +0530

    React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
+import React from 'react'
 import './App.css';
-import { useEffect, useState } from 'react';
-function App() {
-  const [data, setData] = useState(null);
-  useEffect(()=>{
-    fetch(
-      `https://newsapi.org/v2/everything?q=tesla&from=2023-05-17&sortBy=publishedAt&apiKey=12b57f3f5a6b4ba0a04e4df8a2627c02`
-    )
-    .then((response)=> response.json())
-    .then(setData);
-  },[]);
-if(data)
+import { Link } from 'react-router-dom';
+export function Home(){
   return(
-    <pre>{JSON.stringify(data,null,2)}</pre>
+    <div>
+      <nav>
+        <Link to="/Home">Home</Link>
+        <Link to="/About">About</Link>
+        
+      </nav>
+    <h1>Home</h1>
+  </div>
   )
-  return (
-    <>
-    <h1>
-      Data
-    </h1>
-    
-    </>
-  );
+  
+
 }
 
-export default App;
+export function About(){
+
+return(
+  <div>
+    <nav>
+        <Link to="/Home">Home</Link>
+        <Link to="/About">About</Link>
+        
+      </nav>
+    <h1>About</h1>
+  </div>
+)
+
+}
+
+
+
+export function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
